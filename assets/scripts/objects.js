43cb5d9524d3f22eea0102a9bede0d19d5c69bad
dejotb <dejotb@gmail.com>
2022-02-21 19:14:11 +0100

    added movies rednering function and filtering function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,7 +1,36 @@
 const addMovieBtn = document.querySelector('#add-movie-btn');
 const searchMovieBtn = document.querySelector('#search-btn');
+const moviesList = document.querySelector('#movie-list');
 
 const movies = [];
+
+const renderMovie = function (filter = '') {
+  if (movies.length === 0) {
+    moviesList.classList.remove('visible');
+  } else {
+    moviesList.classList.add('visible');
+    moviesList.innerHTML = '';
+
+    const filteredMovies = !filter
+      ? movies
+      : movies.filter((movie) =>
+          movie.info.title.toLowerCase().includes(filter.toLowerCase())
+        );
+
+    filteredMovies.forEach((movie) => {
+      let html = `${movie.info.title} - `;
+      for (const key in movie.info) {
+        if (key != 'title') {
+          html += `${key}: ${movie.info[key]}`;
+        }
+      }
+
+      const text = `<li>${html}</li>`;
+
+      moviesList.insertAdjacentHTML('beforeend', text);
+    });
+  }
+};
 
 const addMovieHandler = function () {
   const title = document.querySelector('#title').value;
@@ -13,19 +42,35 @@ const addMovieHandler = function () {
     extraName.trim() === '' ||
     extraValue.trim() === ''
   ) {
-    console.log('empty field');
+  } else {
+    const newMovie = {
+      info: {
+        title,
+        [extraName]: extraValue,
+      },
+      id: Math.floor(Math.random() * 1000),
+    };
+
+    movies.push(newMovie);
+    renderMovie();
   }
 
-  const newMovie = {
-    info: {
-      title,
-      [extraName]: extraValue,
-    },
-    id: Math.floor(Math.random() * 1000),
-  };
-
-  movies.push(newMovie);
-  console.log(movies);
+  document.querySelector('#title').value = '';
+  document.querySelector('#extra-name').value = '';
+  document.querySelector('#extra-value').value = '';
 };
 
 addMovieBtn.addEventListener('click', addMovieHandler);
+
+const searchMovieHandler = function () {
+  const filterTitle = document.querySelector('#filter-title').value;
+  // const filteredMovies = movies.filter((movie) =>
+  //   movie.info.title.toLowerCase().includes(filterTitle.toLowerCase())
+  // );
+
+  // console.log(filteredMovies);
+
+  renderMovie(filterTitle);
+};
+
+searchMovieBtn.addEventListener('click', searchMovieHandler);
